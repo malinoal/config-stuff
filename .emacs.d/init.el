7199ae6bf85f7e575a27e7924473cb8ad3f2c3fe
malinoal <a.malinowski@campus.tu-berlin.de>
2022-06-19 10:17:48 +0200

    Emacs LSP

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,7 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
-;; (package-refresh-contents)
+;;(package-refresh-contents)
 
 ;; Download Evil
 (unless (package-installed-p 'evil)
@@ -12,16 +12,64 @@
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
+
+;; Download lsp stuff
+(unless (package-installed-p 'lsp-mode)
+  (package-install 'lsp-mode))
+(unless (package-installed-p 'lsp-ui)
+  (package-install 'lsp-ui))
+(unless (package-installed-p 'company)
+  (package-install 'company))
+(unless (package-installed-p 'lsp-java)
+  (package-install 'lsp-java))
+(unless (package-installed-p 'typescript-mode)
+  (package-install 'typescript-mode))
+(unless (package-installed-p 'editorconfig)
+  (package-install 'editorconfig))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (evil))))
+ '(package-selected-packages
+   '(editorconfig typescript-mode ts ## company lsp-ui lsp-java lsp-mode evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; LSP support
+(require 'lsp-mode)
+(add-hook 'js-mode-hook #'lsp)
+(add-hook 'ts-mode-hook #'lsp)
+(add-hook 'typescript-mode-hook #'lsp)
+(add-hook 'html-mode-hook #'lsp)
+;;(add-hook 'prog-mode-hook #'lsp)
+(require 'lsp-java)
+(add-hook 'java-mode-hook #'lsp)
+;; Angular LSP
+(setq lsp-clients-angular-language-server-command
+  '("node"
+    "/usr/lib/node_modules/@angular/language-server"
+    "--ngProbeLocations"
+    "/usr/lib/node_modules"
+    "--tsProbeLocations"
+    "/usr/lib/node_modules"
+    "--stdio"))
+
+;; give emacs more resources
+(setq gc-cons-threshold 500000000)
+(setq read-process-output-max (* 1024 1024 5)) ;; 5mb
+
+
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+(setq visible-bell 1)
