810bfe15cb54f59ea787689f53056ef9ea2607fa
malinoal <a.malinowski@campus.tu-berlin.de>
2022-08-28 08:55:43 +0200

    Add dwm config

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+cp ~/.vimrc .
+cp ~/.emacs.d/init.el .emacs.d/init.el
+cp -r ~/.vim/ .
+cp ~/Documents/dev/dwm/config.h dwm/config.h
+cp ~/Documents/dev/dmenu/config.h dmenu/config.h
+cp ~/Documents/dev/slstatus/config.h slstatus/config.h
