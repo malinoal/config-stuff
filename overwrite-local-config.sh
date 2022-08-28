ea5b0ee1b57a91d658d30188f74194fd79385c9b
malinoal <a.malinowski@campus.tu-berlin.de>
2022-08-28 09:35:11 +0200

    Add script to apply config

---
+++ overwrite-local-config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+cp .vimrc ~/.vimrc 
+cp .emacs.d/init.el ~/.emacs.d/init.el 
+cp dwm/config.h ~/Documents/dev/dwm/config.h 
+cp dmenu/config.h ~/Documents/dev/dmenu/config.h 
+cp slstatus/config.h ~/Documents/dev/slstatus/config.h 
