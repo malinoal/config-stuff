4172124dfc87863f8ecc5af0539b97017c75a5f1
malinoal <a.malinowski@campus.tu-berlin.de>
2022-11-11 09:08:44 +0100

    Add multi-monitor support to dwm

--- overwrite-local-config.sh
+++ overwrite-local-config.sh
@@ -3,5 +3,7 @@ cp .vimrc ~/.vimrc
 cp .emacs.d/init.el ~/.emacs.d/init.el 
 cp .bashrc ~/.bashrc 
 cp dwm/config.h ~/Documents/dev/dwm/config.h 
+cp dwm/dwm.c ~/Documents/dev/dwm/dwm.c 
+cp dwm/config.h ~/Documents/dev/dwm/config.h 
 cp dmenu/config.h ~/Documents/dev/dmenu/config.h 
 cp slstatus/config.h ~/Documents/dev/slstatus/config.h 
--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ cp ~/.vimrc .
 cp ~/.emacs.d/init.el .emacs.d/init.el
 cp -r ~/.vim/ .
 cp ~/Documents/dev/dwm/config.h dwm/config.h
+cp ~/Documents/dev/dwm/dwm.c dwm/dwm.c
 cp ~/Documents/dev/dmenu/config.h dmenu/config.h
 cp ~/Documents/dev/slstatus/config.h slstatus/config.h
 cp ~/.bashrc .
